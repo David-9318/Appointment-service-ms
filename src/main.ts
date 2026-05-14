86e1ef206c7ab0679f406722f775a71a5273ff2d
yeval <226636545+yvalencia-ucp@users.noreply.github.com>
2026-05-13 22:35:46 -0500

    setup

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { getValidationPipe } from './common/validation/get-validation-pipe';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(getValidationPipe());
-  await app.listen(process.env.API_PORT ?? 3000);
+  await app.listen(process.env.API_PORT ?? process.env.PORT ?? 3000);
 }
 bootstrap();
